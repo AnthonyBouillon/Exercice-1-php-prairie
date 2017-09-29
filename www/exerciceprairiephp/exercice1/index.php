78b6fe4809834ea51f9eb14805f2b6b24acd4930
Anthony <anthony_bouillon@outlook.fr>
2017-09-29 16:09:05 +0200

    Exercice 1 prairie php

--- www/exerciceprairiephp/exercice1/index.php
+++ www/exerciceprairiephp/exercice1/index.php
@@ -1,3 +1,11 @@
 <?php
-	echo "Hello world";
+	$chiffre = 20;
+	$chiffre2= 20;
+
+	$temp = $chiffre + $chiffre2;
+	echo "20 + 20 = $temp <br>";
+
+	$temp2 = $chiffre * $chiffre2;
+	echo "20 x 20 = $temp2";
+	
 ?>
